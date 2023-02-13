3926440f51584264bac010213b88f2311f444ce9
Edvard Englund <edvard.englund@medsci.uu.se>
2023-02-13 16:18:18 +0100

    greeting controller

---
+++ src/main/java/com/example/conference/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.example.conference.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+    @GetMapping("greeting")
+    public String greeting(Map<String, Object> model) {
+        model.put("message", "Hello Edvard!");
+        return "greeting";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/greeting.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Greeting</title>
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <h1>${message}</h1>
+  </body>
+</html>
